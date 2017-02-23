359d2b2cc8a2efda068e5891410673d9d51bf441
er1an9 <sun_eagle@ymail.com>
2017-02-23 11:12:54 +0700

    add new

--- index.php
+++ index.php
@@ -1,99 +1,261 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>SISWA</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Metamorphosis Design Free Css Templates</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
 
-    <!-- Bootstrap -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+<!-- Begin JavaScript -->
 
-    <link rel="stylesheet" href="assets/style.css">
+	<script type="text/javascript" src="lib/jquery-1.3.2.min.js"></script>
+	<script type="text/javascript" src="lib/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="lib/jquery.coda-slider-2.0.js"></script>
+<!-- Initialize each slider on the page. Each slider must have a unique id -->
+	<script type="text/javascript">
+	$().ready(function() {
+       $('#coda-slider-2').codaSlider({
+           autoSlide: true,
+           autoSlideInterval: 6000,
+           autoSlideStopWhenClicked: true
+
+       });
+   });
+</script>
+	<!-- End JavaScript -->
+
+<!-- Pirobox setup and styles -->
+
+<script type="text/javascript" src="lib/pirobox.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$().piroBox({
+			my_speed: 400, //animation speed
+			bg_alpha: 0.1, //background opacity
+			slideShow : false, // true == slideshow on, false == slideshow off
+			slideSpeed : 4, //slideshow duration in seconds(3 to 6 Recommended)
+			close_all : '.piro_close,.piro_overlay'// add class .piro_overlay(with comma)if you want overlay click close piroBox
+
+	});
+});
+</script>
+
+<link href="images/style.css" rel="stylesheet" type="text/css" />
+
+<!-- Pirobox setup and styles end-->
+
   </head>
-  <body style="bgcolor=red;">
+  <body>
 
-    <div class="container">
+    <div id="bg_img">
+      <div id="site_div">
+        <div id="header">
+          <div id="logo">
+              <h1><a href="#">metamorph_classified</a></h1>
+              <a href="#"><small>Design by Metamorphosis Design</small></a>
+          </div>
+          <div id="menu">
+              <ul>
+                  <li><a href="index.html" class="active">Home</a></li>
+                  <li><a href="blog.html">Blog</a></li>
+                  <li><a href="gallery.html">Gallery</a></li>
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="contact.html">Contact Us</a></li>
+              </ul>
+          </div>
+          <div style="clear: both;"></div>
+        </div>
+        <div id="main">
+          <div class="index_main_top"></div>
+          <div class="main_bg">
+            <div class="index_prev_grad">
 
-      <!-- Bagian Header -->
-      <div class="row">
-          <div class="col-md-12 header" id="site-header">
-              <!-- isi header -->
-              <header>
-                <h1 class="title-site">Sistem Informasi Salt Water Aquarium</h1>
-                <p class="description">Template Website Dengan Bootstrap</p>
-              </header>
-              <nav class="menus">
-                  <ul>
-                      <li><a href="#">Home</a></li>
-                      <li><a href="#">Abouts Us</a></li>
-                      <li><a href="#">Portfolio</a></li>
-                  </ul>
-              </nav>
+              <div class="coda-slider-wrapper">
+                <div class="coda-slider preload" id="coda-slider-2">
+                  <div class="panel">
+                    <div class="panel-wrapper">
+
+                      <div class="prev_but_center">
+                        <div class="prev_but_center_left">
+                          <div class="prec_img"><img src="images/pic-01.jpg" alt="" title="" /></div>
+                        </div>
+                        <div class="prev_but_center_right">
+                          <p><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </a><br />
+                              Nam justo odio, congue id fermentum sit amet, placerat sed nibh. Vestibulum pharetra nibh eu magna accumsan tristique. Sed ac velit urna, venenatis vulputate purus.</p>
+                          <div class="read"><a href="#">read more</a></div>
+                          <p><a href="#">Vivamus id lobortis nisi. Duis semper porta justo, vel sodales velit vehicula vel. </a><br />
+                              Donec non vulputate odio. Quisque tellus augue, tristique vel lobortis ut, convallis vel est. Nam vel congue lectus. Suspendisse ullamcorper odio et leo congue eu semper </p>
+                          <div class="read"><a href="#">read more</a></div>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class="panel">
+              			<div class="panel-wrapper">
+
+              				<div class="prev_but_center">
+                        <div class="prev_but_center_left">
+                          <div class="prev_img"><img src="images/header_scroll.jpg" alt="" title=""/></div>
+                        </div>
+                        <div class="prev_but_center_right">
+                          <p><a href="#">Vivamus id lobortis nisi. Duis semper porta justo, vel sodales velit vehicula vel. </a><br />
+                              Donec non vulputate odio. Quisque tellus augue, tristique vel lobortis ut, convallis vel est. Nam vel congue lectus. Suspendisse ullamcorper odio et leo congue eu semper  Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /><br />
+                              Nam justo odio, congue id fermentum sit amet, placerat sed nibh. Vestibulum pharetra nibh eu magna accumsan tristique. Sed ac velit urna, venenatis vulputate purus.</p>
+                          <div class="read"><a href="#">read more</a></div>
+                        </div>
+                      </div>
+
+              			</div>
+              		</div>
+                  <!-- ./panel -->
+
+              		<div class="panel">
+              			<div class="panel-wrapper">
+
+              				<div class="prev_but_center">
+                        <div class="prev_but_center_left">
+                          <div class="prev_img"><img src="images/header_scroll2.jpg" alt="" title=""/></div>
+                        </div>
+                        <div class="prev_but_center_right">
+                          <p><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </a><br />
+                              Nam justo odio, congue id fermentum sit amet, placerat sed nibh. Vestibulum pharetra nibh eu magna accumsan tristique. Sed ac velit urna, venenatis vulputate purus.</p>
+                          <div class="read"><a href="#">read more</a></div>
+                          <p><a href="#">Vivamus id lobortis nisi. Duis semper porta justo, vel sodales velit vehicula vel. </a><br />
+                              Donec non vulputate odio. Quisque tellus augue, tristique vel lobortis ut, convallis vel est. Nam vel congue lectus. Suspendisse ullamcorper odio et leo congue eu semper </p>
+                          <div class="read"><a href="#">read more</a></div>
+                        </div>
+                      </div>
+
+              			</div>
+              		</div>
+                  <!-- ./panel -->
+
+                  <div class="panel">
+              			<div class="panel-wrapper">
+
+              				<div class="prev_but_center">
+                        <div class="prev_but_center_left">
+                          <div class="prev_img"><img src="images/header_scroll3.jpg" alt="" title=""/></div>
+                        </div>
+                        <div class="prev_but_center_right">
+                          <p><a href="#">Vivamus id lobortis nisi. Duis semper porta justo, vel sodales velit vehicula vel. </a><br />
+                                Donec non vulputate odio. Quisque tellus augue, tristique vel lobortis ut, convallis vel est. Nam vel congue lectus. Suspendisse ullamcorper odio et leo congue eu semper  Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /><br />
+                                Nam justo odio, congue id fermentum sit amet, placerat sed nibh. Vestibulum pharetra nibh eu magna accumsan tristique. Sed ac velit urna, venenatis vulputate purus.</p>
+                          <div class="read"><a href="#">read more</a></div>
+                        </div>
+                      </div>
+
+              			</div>
+              		</div>
+                  <!-- ./panel -->
+
+                </div> <!-- .coda-slidebar -->
+              </div>
+
             </div>
-      </div>
-      <!-- End Bagian Header -->
+            <div class="index_prev_bot"></div>
+            <div id="index_box">
+                <div id="index_box_top"></div>
+                <div id="index_box_bg">
+                    <h3>Etiam et mi arcu, at aliquet sapien. Proin mauris mauris, aliquet ut pellentesque eu, fringilla quis neque.</h3>
+                    <p>Curabitur euismod venenatis odio, eu mollis augue faucibus sit amet. Curabitur aliquet varius dui ut lacinia. In hac habitasse platea dictumst. Donec dictum velit nec leo dignissim eget pellentesque elit hendrerit. In hac habitasse platea dictumst. Etiam et mi arcu, at aliquet sapien. Proin mauris mauris, aliquet ut pellentesque eu, fringilla </p>
+                    <div class="readred"><a href="#">read more</a></div>
+                </div>
+                <div id="index_box_bot"></div>
+            </div>
 
-      <!-- Bagian Wrapper 2 kolom -->
-      <div class="row">
-          <div class="col-md-8 articles" id="site-content">
-             <!-- isi content -->
-
-            <article class="posts">
-              <h2 class="title-post">Lorem ipsum dolor sit amet</h2>
-              <div class="meta-post">
-                  <span><em class="glyphicon glyphicon-user"></em> Onphpid</span>
-                  <span><em class="glyphicon glyphicon-time"></em> 13 Juni 2015</span>
+            <div id="index_col">
+              <div id="index_col1">
+                <img src="images/notebook.png" alt="" title="" style="float: left; padding-right: 10px; padding-bottom: 4px;" />
+                <h4>Company News</h4>
+                <div class="pad" style="padding-bottom: 7px; border-bottom: 1px dotted #666666; margin-bottom: 10px;">
+                  <a href="#">Apr. 10, 2010</a><br />
+                  <p><a href="#">Suspendisse rutrum interdum lacinia. </a><br />
+                      Suspendisse tempus aliquet elit sit amet pellentesque. Donec iaculis pulvinar mauris, </p>
+                  <div style="text-align: right"><a href="#">read more...</a></div>
+                </div>
+                <div class="pad" style="padding-bottom: 3px;">
+                  <a href="#">Apr. 10, 2010</a><br />
+                  <p><a href="#">Suspendisse rutrum interdum lacinia. </a><br />
+                      Suspendisse tempus aliquet elit sit amet pellentesque. Donec iaculis pulvinar mauris, </p>
+                  <div style="text-align: right"><a href="#">read more...</a></div>
+                </div>
               </div>
-              <div class="content">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
+              <div id="index_col2">
+                <img src="images/green.png" alt="" title="" style="float: left; padding-right: 10px; padding-bottom: 4px;" />
+                <h4>Recent Projects</h4><br />
+                <a href="images/project1.jpg" class="pirobox_footer" title="First Project Screenshot"><img src="images/pic-02.jpg" alt="" title=""/></a><br /><br />
+                <a href="images/project2.jpg" class="pirobox_footer" title="Second Project Screenshot"><img src="images/pic-03.jpg" alt="" title=""/></a>
               </div>
-            </article>
+              <div id="index_col3">
+                <img src="images/contact.png" alt="" title="" style="float: left; padding-right: 10px; padding-bottom: 4px;" />
+                <h4>Get a Free Quote</h4>
+                <div id="log">
+                  <form id="form1" method="post" action="#">
+                    <fieldset>
+                      <input id="text1" type="text" name="text1" value="Name" alt=""/><br />
+                      <input id="text2" type="text" name="text2" value="E-mail" alt=""/><br />
+                      <input id="text3" type="text" name="text3" value="Subject" alt=""/><br />
+                      <textarea id="text_mess" name="text_mess" cols="0" rows="0">Message</textarea><br />
+                      <input type="submit" id="login-submit" value="Send"/>
+                    </fieldset>
+                  </form>
+                </div>
+              </div>
+              <div style="clear: both"></div>
+            </div>
 
           </div>
-          <div class="col-md-4 sidebar" id="site-sidebar">
-              <!-- isi sidebar -->
-              <aside class="widgets">
-                <h3 class="widget-title">Latest Posts</h3>
-                <ul>
-                    <li><a href="#">Lorem Ipsum</a></li>
-                    <li><a href="#">Lorem Ipsum</a></li>
-                    <li><a href="#">Lorem Ipsum</a></li>
-                    <li><a href="#">Lorem Ipsum</a></li>
-                    <li><a href="#">Lorem Ipsum</a></li>
-                </ul>
-              </aside>
-          </div>
-      </div>
-      <!-- End Bagian wrapper -->
+          <div class="main_bot"></div>
+        </div>
 
-      <!-- Bagian footer -->
-      <div class="row">
-          <div class="col-md-12 footer" id="site-footer">
-              <!-- isi footer -->
-              <footer class="copyright text-center"><p>&copy; 2017 SISWA</p></footer>
-          </div>
       </div>
-      <!-- End Bagian footer -->
+
+      <div id="footer">
+        <div class="foot_col">
+          <div class="foot_col1">
+            <h2>Useful Resources</h2>
+            <div class="foot_pad">
+              <ul class="ls">
+                <li><a href="#">Free Website Templates</a></li>
+                <li><a href="#">Free Flash Templates</a></li>
+                <li><a href="#">Easy Flash Templates</a></li>
+                <li><a href="#">Top Hosting Providers</a></li>
+                <li><a href="#">Free Fonts</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="foot_col2">
+            <h2>Share with Others</h2>
+            <div class="foot_pad">
+              <div class="link1"><a href="#">Subscribe to Blog</a></div>
+              <div class="link2"><a href="#">Be a fan on Facebook</a></div>
+              <div class="link3"><a href="#">RSS Feed</a></div>
+              <div class="link4"><a href="#">Follow us on Twitter</a></div>
+            </div>
+          </div>
+
+          <div class="foot_col3">
+            <h2>Our Address</h2>
+            <div class="foot_pad">
+              <a href="#"><img src="images/location.jpg" alt="" title="" style="padding-top: 5px;"/></a>
+            </div>
+          </div>
+          <div style="clear: both"></div>
+        </div>
+
+
+        <div id="footer_bot">
+            <p>Copyright  2015. <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a> </p>
+<!-- Please DO NOT remove the following notice --><div class="foot_lnk"><p>Website Design <a href="http://www.metamorphozis.com/" class="cop_lnk" title="Free Website Templates">Free Website Templates</a></p><!-- end of copyright notice-->
+        </div>
+      </div>
 
     </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
-    <script src="assets/js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="assets/js/bootstrap.min.js"></script>
-
-    <!-- Custom Js -->
   </body>
 </html>
