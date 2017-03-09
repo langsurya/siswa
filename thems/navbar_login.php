f2ae325e1930d74ed748c581285964ad53ac2a71
er1an9 <sun_eagle@ymail.com>
2017-03-10 03:41:07 +0700

    update frond end member

---
+++ thems/navbar_login.php
@@ -0,0 +1,51 @@
+<?php
+if (isset($_POST['login'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  if ($login->ceklogin($username,$password)) {
+    echo "<script>alert('Anda berhasil Login');</script>";
+    echo "<meta http-equiv='refresh' content='0; url=index.php'>";
+  }else{
+    echo "<script>alert('Anda Gagal Login');</script>";
+    echo "<meta http-equiv='refresh' content='0; url=index.php'>";
+  }
+}
+?>
+<div class="leftbar leftbar-close clearfix">
+  <div class="responsive-leftbar"> <i class="icon-list"></i> </div>
+    <div class="left-secondary-nav">
+      <div>
+        <h4 class="side-head">Login Member</h4>
+        <div class="span2"><br>
+          <form action="" method="POST">
+            <div class="control-group">
+              <div class="controls input-icon"> <i class="icon-envelope"></i>
+                <input type="text" name="username" placeholder="username" class="span2">
+              </div>
+            </div>
+            <div class="control-group">
+              <div class="controls input-icon"> <i class="icon-key"></i>
+                <input type="password" name="password" placeholder="Password" class="span2">
+              </div>
+            </div>
+            <div class="control-group">
+              <!-- <div class="controls"> -->
+                <button type="submit" name="login" class="btn btn-primary">Sign in</button>
+                <button type="button" class="btn btn-danger ">Cancle</button>
+              <!-- </div> -->
+            </div>
+          </form>
+        </div>
+        <h4 class="side-head">Forms</h4>
+        <ul id="nav" class="accordion-nav">
+          <li><a href="index.php"><i class=" icon-home"></i> Home </a></li>
+          <li><a href="?menu=daftar"><i class=" icon-user"></i> Daftar Member </a></li>
+        </ul>
+
+        <h4 class="side-head">Features</h4>
+      </div>
+    </div>
+  </div>
+
+
+</div>
